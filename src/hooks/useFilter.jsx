16efa1eacea2b0ac98c2f652df20597553129d43
RiaJul Pro <riajulpro@outlook.com>
2023-11-08 12:04:18 +0600

    Adding Filter System and Showing the file

---
+++ src/hooks/useFilter.jsx
@@ -0,0 +1,16 @@
+import { useQuery } from "@tanstack/react-query";
+
+const useFilter = () => {
+  const { data, isLoading, isFetching, refetch } = useQuery({
+    queryKey: ["filterData"],
+    queryFn: async () => {
+      const data = await fetch(
+        `http://localhost:5000/filter`
+      );
+      return await data.json();
+    },
+  });
+  return { data, isLoading, isFetching, refetch };
+};
+
+export default useFilter;
--- src/pages/public/AllAssignment.jsx
+++ src/pages/public/AllAssignment.jsx
@@ -6,6 +6,7 @@ import axios from "axios";
 import useAssignments from "../../hooks/useAssignments";
 import { AuthContext } from "../../context/Authentication";
 import Swal from "sweetalert2";
+import useFilter from "../../hooks/useFilter";
 
 const AllAssignment = () => {
   const { count } = useLoaderData();
@@ -13,12 +14,23 @@ const AllAssignment = () => {
   const item = 9;
   const pages = [...Array(Math.ceil(count / item)).keys()];
   const [currentPage, setCurrentPage] = useState(0);
+  const [filter, setFilter] = useState("");
 
   const {
     data: assignments,
     isLoading,
     refetch,
   } = useAssignments(currentPage, item);
+
+  const { data: filteredData, isLoading: filterLoading } = useFilter();
+
+  const toShow = !filter
+    ? assignments
+    : filteredData.filter((data) => data.level === filter);
+
+  const handleFilterChange = (e) => {
+    setFilter(e.target.value);
+  };
 
   const deleteAnItem = (id, email) => {
     if (user.email === email) {
@@ -57,9 +69,7 @@ const AllAssignment = () => {
     }
   };
 
-  // const { data: assignments, isLoading, refetch } = useAssignments();
-
-  if (isLoading) {
+  if (isLoading || filterLoading) {
     return (
       <div className="h-[calc(100vh-100px)] flex justify-center items-center">
         <Loading className="w-96 h-96" />
@@ -69,8 +79,21 @@ const AllAssignment = () => {
 
   return (
     <>
-      <div className="md:w-9/12 mx-auto grid gap-2 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 my-5">
-        {assignments?.map((ass) => (
+      <div className="md:w-9/12 mx-auto flex items-center justify-end my-5">
+        <span className="font-bold mr-2">Filter By: </span>
+        <select
+          value={filter}
+          onChange={handleFilterChange}
+          className="border p-1"
+        >
+          <option value="">All</option>
+          <option value="easy">Easy</option>
+          <option value="medium">Medium</option>
+          <option value="hard">Hard</option>
+        </select>
+      </div>
+      <div className="md:w-9/12 mx-auto grid gap-2 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        {toShow?.map((ass) => (
           <div key={ass._id} className="bg-gray-100 rounded-md p-2">
             <p>
               <img src={ass?.thumbnail} alt="" className="rounded-md" />
@@ -102,45 +125,49 @@ const AllAssignment = () => {
         ))}
       </div>
 
-      <div className="flex justify-center my-5">
-        <div>
-          <button
-            className="bg-gray-100 p-2 border-r"
-            onClick={() => {
-              if (currentPage > 0) {
-                setCurrentPage(currentPage - 1);
-              }
-            }}
-          >
-            Prev
-          </button>
-          {pages.map((page) => (
-            <button
-              key={page}
-              className={
-                currentPage === page
-                  ? "bg-orange-400 text-white p-2 border-r"
-                  : "bg-gray-100 p-2 border-r"
-              }
-              onClick={() => {
-                setCurrentPage(page);
-              }}
-            >
-              {page}
-            </button>
-          ))}
-          <button
-            className="bg-gray-100 p-2 border-r"
-            onClick={() => {
-              if (currentPage < pages.length - 1) {
-                setCurrentPage(currentPage + 1);
-              }
-            }}
-          >
-            Next
-          </button>
-        </div>
-      </div>
+      {!filter && (
+        <>
+          <div className="flex justify-center my-5">
+            <div>
+              <button
+                className="bg-gray-100 p-2 border-r"
+                onClick={() => {
+                  if (currentPage > 0) {
+                    setCurrentPage(currentPage - 1);
+                  }
+                }}
+              >
+                Prev
+              </button>
+              {pages.map((page) => (
+                <button
+                  key={page}
+                  className={
+                    currentPage === page
+                      ? "bg-orange-400 text-white p-2 border-r"
+                      : "bg-gray-100 p-2 border-r"
+                  }
+                  onClick={() => {
+                    setCurrentPage(page);
+                  }}
+                >
+                  {page}
+                </button>
+              ))}
+              <button
+                className="bg-gray-100 p-2 border-r"
+                onClick={() => {
+                  if (currentPage < pages.length - 1) {
+                    setCurrentPage(currentPage + 1);
+                  }
+                }}
+              >
+                Next
+              </button>
+            </div>
+          </div>
+        </>
+      )}
     </>
   );
 };
