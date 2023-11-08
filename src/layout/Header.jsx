2ebc0ec109790d9499d3bb58375896c2b591fada
RiaJul Pro <riajulpro@outlook.com>
2023-11-08 08:36:30 +0600

    Updating the navbar with user conditional

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -4,12 +4,12 @@ import Navbar from "./Navbar";
 const Header = () => {
   return (
     <>
-      <header className="hidden md:flex justify-between items-center bg-gray-200 md:px-10 md:py-4">
+      <header className="hidden lg:flex justify-between items-center bg-[#1D3557] text-white md:px-10 md:py-4">
         <div className="hidden lg:block font-bold text-2xl">StudyHub</div>
         <Navbar />
         {/* <User /> */}
       </header>
-      <header className="bg-gray-50 lg:hidden sticky top-0">
+      <header className="bg-[#1D3557] text-white lg:hidden sticky top-0">
         <div className="w-9/12 mx-auto p-3">
           <Navbar />
         </div>
--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -9,8 +9,12 @@ import {
 import { TbListDetails } from "react-icons/tb";
 import { MdBookmarkAdded, MdAssignmentTurnedIn } from "react-icons/md";
 import { BiSolidUser } from "react-icons/bi";
+import { useContext } from "react";
+import { AuthContext } from "../context/Authentication";
 
 const Navbar = () => {
+  const { user } = useContext(AuthContext);
+
   return (
     <nav className="flex justify-between md:justify-center md:gap-3">
       <NavLink to={"/"} className="flex gap-2 items-center">
@@ -21,22 +25,37 @@ const Navbar = () => {
         <TbListDetails />
         <span className="hidden md:block">All Assignment</span>
       </NavLink>
-      <NavLink to={"/my-assignment"} className="flex gap-2 items-center">
-        <MdAssignmentTurnedIn />
-        <span className="hidden md:block">My Assignment</span>
-      </NavLink>
-      <NavLink to={"/create-assignment"} className="flex gap-2 items-center">
-        <AiFillFileAdd />
-        <span className="hidden md:block">Create Assignment</span>
-      </NavLink>
-      <NavLink to={"/submitted-assignment"} className="flex gap-2 items-center">
-        <MdBookmarkAdded />
-        <span className="hidden md:block">Submitted Assignment</span>
-      </NavLink>
-      <NavLink to={"/profile"} className="flex gap-2 items-center">
-        <BiSolidUser />
-        <span className="hidden md:block">Profile</span>
-      </NavLink>
+      {user ? (
+        <>
+          <NavLink to={"/my-assignment"} className="flex gap-2 items-center">
+            <MdAssignmentTurnedIn />
+            <span className="hidden md:block">My Assignment</span>
+          </NavLink>
+          <NavLink
+            to={"/create-assignment"}
+            className="flex gap-2 items-center"
+          >
+            <AiFillFileAdd />
+            <span className="hidden md:block">Create Assignment</span>
+          </NavLink>
+          <NavLink
+            to={"/submitted-assignment"}
+            className="flex gap-2 items-center"
+          >
+            <MdBookmarkAdded />
+            <span className="hidden md:block">Submitted Assignment</span>
+          </NavLink>
+          <NavLink to={"/profile"} className="flex gap-2 items-center">
+            <BiSolidUser />
+            <span className="hidden md:block">Profile</span>
+          </NavLink>
+        </>
+      ) : (
+        <NavLink to={"/login"} className="flex gap-2 items-center">
+          <BiSolidUser />
+          <span className="hidden md:block">Login</span>
+        </NavLink>
+      )}
     </nav>
   );
 };
